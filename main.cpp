fdd1644334c85e974a77bf93027582855a8c81f5
Reda Marzouk <kakolaukiom19@gmail.com>
2017-11-20 22:49:15 +0000

    Clean main final

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "Histogram.h"
-#include "FrameHist.h"
 #include "FenetrePrincipale.h"
-#include <iostream>
-using namespace cv;
-using namespace std;
 
 int main(int argc, char *argv[])
 {
